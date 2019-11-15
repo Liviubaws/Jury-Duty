82ecaca2957cb1831f4b71043ab9e456d3fe7b2f
Bogdan Codreanu <bogdan.codreanu@uipath.com>
2019-11-15 09:43:20 +0200

    cosmetic

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -2,89 +2,94 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.page.html',
-  styleUrls: ['./admin.page.scss'],
+    selector: 'app-admin',
+    templateUrl: './admin.page.html',
+    styleUrls: ['./admin.page.scss'],
 })
 export class AdminPage implements OnInit {
-created:boolean;
-extended:boolean;
-contests = [];
-contest;
-juror;
-organiser;
-name:string;
-jurors = [];
-organisers = [];
-organisersNumber:number;
-jurorsNumber:number;
-organisersNames = [];
-jurorsNames = [];
-  constructor(public fdb: AngularFireDatabase) {
-    this.fdb.list("/contests/").valueChanges().subscribe(__contests => {
-      this.contests = __contests;
-    });
-    this.fdb.list("/organisers/").valueChanges().subscribe(__organisers => {
-      this.organisers = __organisers;
-    });
-    this.fdb.list("/jurors/").valueChanges().subscribe(__jurors => {
-      this.jurors = __jurors;
-    });
-    this.created = false;
-    this.extended = false;
-  }
+    created: boolean;
+    extended: boolean;
+    contests: any[] = [];
+    contest;
+    juror;
+    organiser;
+    name: string;
+    jurors = [];
+    organisers = [];
+    organisersNumber: number;
+    jurorsNumber: number;
+    organisersNames = [];
+    jurorsNames = [];
+    constructor(public fdb: AngularFireDatabase) {
+        this.fdb.list("/contests/").valueChanges().subscribe(__contests => {
+            this.contests = __contests;
+        });
+        this.fdb.list("/organisers/").valueChanges().subscribe(__organisers => {
+            this.organisers = __organisers;
+        });
+        this.fdb.list("/jurors/").valueChanges().subscribe(__jurors => {
+            this.jurors = __jurors;
+        });
+        this.created = false;
+        this.extended = false;
+    }
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
-  create() {
-    this.created = true; 
-  }
-  extend(){
-    this.extended = true;
-    this.organisers.length = this.organisersNumber;
-    this.jurors.length = this.jurorsNumber;
-    this.organisersNames.length = this.organisersNumber;
-    this.jurorsNames.length = this.jurorsNumber;
-  }
-  save(){
-    for(var i = 0; i < this.jurorsNumber; i++){
-      var code = this.generateCode();
-      this.jurors[i] = {
-        number: i,
-        name: this.jurorsNames[i],
-        jurorCode: code
-      }
     }
-    for(var i = 0; i < this.organisersNumber; i++){
-      var code = this.generateCode();
-      this.organisers[i] = {
-        number: i,
-        name: this.organisersNames[i],
-        organiserCode: code
-      }
+    create() {
+        this.created = true;
     }
-    console.log(this.organisers);
-    console.log(this.jurors);
-    this.contest = {
-      name: this.name,
-      jurorsNumber: this.jurorsNumber,
-      organisersNumber: this.organisersNumber,
-      jurors: this.jurors,
-      organisers: this.organisers
+    extend() {
+        this.extended = true;
+        this.organisers.length = this.organisersNumber;
+        this.jurors.length = this.jurorsNumber;
+        this.organisersNames.length = this.organisersNumber;
+        this.jurorsNames.length = this.jurorsNumber;
     }
-    console.log(this.contest);
-    this.fdb.list("/contests/").push(this.contest);
-    this.contests.push(this.contest);
-    this.created = false;
-  }
-  generateCode(){
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < 10; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    save() {
+        for (var i = 0; i < this.jurorsNumber; i++) {
+            var code = this.generateCode();
+            this.jurors[i] = {
+                number: i,
+                name: this.jurorsNames[i],
+                jurorCode: code
+            }
+        }
+        for (var i = 0; i < this.organisersNumber; i++) {
+            var code = this.generateCode();
+            this.organisers[i] = {
+                number: i,
+                name: this.organisersNames[i],
+                organiserCode: code
+            }
+        }
+        console.log(this.organisers);
+        console.log(this.jurors);
+        this.contest = {
+            name: this.name,
+            jurorsNumber: this.jurorsNumber,
+            organisersNumber: this.organisersNumber,
+            jurors: this.jurors,
+            organisers: this.organisers
+        }
+        console.log(this.contest);
+        this.fdb.list("/contests/").push(this.contest);
+        this.contests.push(this.contest);
+        this.created = false;
     }
-    return result;
-  }
+    generateCode() {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < 10; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
+    delete(contest) {
+        this.fdb.list("/contests/").remove(contest);
+        this.contests = this.contests.filter((elem, index, array) => elem != contest);
+    }
 }
